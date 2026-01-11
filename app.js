1b87635e3c2bb131eb67fed548952cc55e7c55e6
Sadiksha KC Neupane <kcneupanesadiksha@gmail.com>
2026-01-11 23:09:16 +0545

    game started

---
+++ app.js
@@ -0,0 +1,12 @@
+let gameSeq = [];
+let userSeq = [];
+let started = false;
+let level = 0;
+
+document.addEventListener("keypress", function () {
+  console.log("game started");
+  if (started == false) {
+    console.log("game started");
+    started = true;
+  }
+});
